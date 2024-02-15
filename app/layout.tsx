ee3ce4d0988442936ab94b8d3586d98654244992
Waseem <wasim_ahmad2003@yahoo.com>
2024-02-15 16:08:57 +0500

    configure aws-amplify

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import React, { ReactNode } from 'react';
 import AppWrappers from './AppWrappers';
 import Providers from '../components/Providers';
 import { verifyRequest } from '../utils/verifyRequest';
+import { amplifyInit } from '../utils/amplifyInit';
 
 export default async function RootLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  amplifyInit();
   const { data, jwtToken }: any = await verifyRequest();
   return (
     <html lang="en">
